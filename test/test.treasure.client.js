52f2877f6f7af45d6190fe29b27a156f140fe9ee
César Andreu <cesarandreu@gmail.com>
2014-06-11 17:31:17 -0700

    refactor(client test): remove helper, follow styleguide

--- test/test.treasure.client.js
+++ test/test.treasure.client.js
@@ -1,20 +1,5 @@
-var expect = chai.expect;
-
-var treasureHelper = {
-  database: 'testDB',
-  writeKey: '91/96da3cfb876cc50724d0dddef670d95eea2a0018',
-  protocol: 'http',
-  host: 'in-staging.treasuredata.com',
-  table: 'testTB',
-  properties: { username: 'treasure', color: 'blue' },
-  responses: {
-    success: '{\"created\": true }',
-    error: '{\"error\": true }'
-  }
-};
-// console.log(treasureHelper);
-
-describe("Treasure Client", function() {
+describe('Treasure Client', function() {
+  var treasure;
 
   beforeEach(function() {
     this.treasure = new Treasure({
@@ -130,27 +115,28 @@ describe("Treasure Client", function() {
 
     describe("validates endpoint", function(){
 
-      it("should default to \"https\" if protocol is absent or of incorrect type", function(){
+      it("should default to autodetection if protocol is absent or of incorrect type", function(){
+        var currentProtocol = location.protocol.replace(/:/g, '');
 
         // Empty
         var treasure_empty = new Treasure({ database: '123', protocol: '' });
-        expect(treasure_empty.client.endpoint.indexOf('https://')).to.equal(0);
+        expect(treasure_empty.client.endpoint.indexOf(currentProtocol)).to.equal(0);
 
         // Number
         var treasure_number = new Treasure({ database: '123', protocol: 0 });
-        expect(treasure_number.client.endpoint.indexOf('https://')).to.equal(0);
+        expect(treasure_number.client.endpoint.indexOf(currentProtocol)).to.equal(0);
 
         // Boolean
         var treasure_boolean = new Treasure({ database: '123', protocol: true });
-        expect(treasure_boolean.client.endpoint.indexOf('https://')).to.equal(0);
+        expect(treasure_boolean.client.endpoint.indexOf(currentProtocol)).to.equal(0);
 
         // Array
         var treasure_array = new Treasure({ database: '123', protocol: [] });
-        expect(treasure_array.client.endpoint.indexOf('https://')).to.equal(0);
+        expect(treasure_array.client.endpoint.indexOf(currentProtocol)).to.equal(0);
 
         // Object
         var treasure_object = new Treasure({ database: '123', protocol: {} });
-        expect(treasure_object.client.endpoint.indexOf('https://')).to.equal(0);
+        expect(treasure_object.client.endpoint.indexOf(currentProtocol)).to.equal(0);
 
       });
 
@@ -172,13 +158,13 @@ describe("Treasure Client", function() {
 
     describe("validates request type", function(){
 
-      it("should set request type to \"jsonp\" by default", function(){
+      it("should set request type to \"xhr\" by default", function(){
 
         var treasure = new Treasure({ database: '123' });
         expect(treasure.client)
           .to.have.property('requestType')
           .that.is.a('string')
-          .that.equals('jsonp');
+          .that.equals('xhr');
 
         /*if ('withCredentials' in new XMLHttpRequest()) {
           expect(treasure.client)
