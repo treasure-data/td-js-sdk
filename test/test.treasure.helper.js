4e3001d4b7e5c2ab5274aa0c80e95c97cb4bb9dd
César Andreu <cesarandreu@gmail.com>
2014-06-11 17:32:14 -0700

    refactor(helper): update values

--- test/test.treasure.helper.js
+++ test/test.treasure.helper.js
@@ -1,9 +1,10 @@
-var treasureHelper = {
-  database: 'testDB',
+window.treasureHelper = {
+  database: 'test_db_1',
   writeKey: '91/96da3cfb876cc50724d0dddef670d95eea2a0018',
   protocol: 'http',
   host: 'in-staging.treasuredata.com',
-  table: 'testTB',
+  jsonphost: 'localhost:9999',
+  table: 'test_tb_1',
   properties: { username: 'treasure', color: 'blue' },
   responses: {
     success: '{\"created\": true }',
