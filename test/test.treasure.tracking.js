4bb23ea64c5a749b0a8a89fcb2adbc2f215c557a
César Andreu <cesarandreu@gmail.com>
2014-06-12 16:34:56 -0700

    test(tracking): update to work with gulp

--- test/test.treasure.tracking.js
+++ test/test.treasure.tracking.js
@@ -1,11 +1,11 @@
-var expect = chai.expect;
+'use strict';
 
-describe("Treasure Tracking", function() {
+describe('Treasure Tracking', function() {
   
-  describe("#addEvent", function() {
+  describe('#addEvent', function() {
     
-    describe("via XHR/CORS (if supported)", function(){
-      
+    describe('via XHR/CORS (if supported)', function () {
+
       beforeEach(function() {
         var self = this;
         self.treasure = new Treasure({
@@ -14,11 +14,11 @@ describe("Treasure Tracking", function() {
           host: treasureHelper.host,
           requestType: 'xhr'
         });
-        self.postUrl = self.treasure.client.endpoint + "/js/v3/event/" + self.treasure.client.database + "/" + treasureHelper.table;
+        self.postUrl = self.treasure.client.endpoint + '/js/v3/event/' + self.treasure.client.database + '/' + treasureHelper.table;
         self.server = sinon.fakeServer.create();
         self.respondWith = function(code, body){
-          self.server.respondWith("POST", self.postUrl, 
-            [code, { "Content-Type": "application/json"}, body]);
+          self.server.respondWith('POST', self.postUrl, 
+            [code, { 'Content-Type': 'application/json'}, body]);
         };
       });
       
@@ -28,7 +28,7 @@ describe("Treasure Tracking", function() {
       
       if ('withCredentials' in new XMLHttpRequest()) {
       
-        it("should post to the API using xhr where CORS is supported", function() {
+        it('should post to the API using xhr where CORS is supported', function() {
           
           var callbacks = [sinon.spy(), sinon.spy()];
           this.respondWith(200, treasureHelper.responses.success);
@@ -61,54 +61,75 @@ describe("Treasure Tracking", function() {
       
     });
   
-    describe("via JSONP to a fake server", function(){
+    describe('via JSONP to a fake server', function () {
       
       beforeEach(function() {
         this.treasure = new Treasure({
           database: treasureHelper.database,
           writeKey: treasureHelper.writeKey,
-          host: treasureHelper.host,
+          host: treasureHelper.jsonphost,
           requestType: 'jsonp'
         });
       });
       
-      it("should add a script tag with a URL that has data and modified params", function(){
+      it('should add a script tag with a URL that has data and modified params', function(){
         
         this.treasure.addEvent(treasureHelper.table, treasureHelper.properties);
-        var tag = document.getElementById("treasure-jsonp");
+        var tag = document.getElementById('td-js-sdk-jsonp');
         expect(tag).to.exist;
-        expect(tag.src).to.contain("data=");
-        expect(tag.src).to.contain("modified=");
-        
+        expect(tag.src).to.contain('data=');
+        expect(tag.src).to.contain('modified=');
+
       });
-      
+
+      it('should call success callback if successful', function (done) {
+        var callbacks = [sinon.spy(), sinon.spy()];
+        this.treasure.addEvent(treasureHelper.table, treasureHelper.properties, callbacks[0], callbacks[1]);
+        setTimeout(function() {
+          expect(callbacks[0].calledOnce).to.be.ok;
+          expect(callbacks[1].calledOnce).not.to.be.ok;
+          expect(callbacks[0].calledWith(JSON.parse(treasureHelper.responses.success))).to.be.ok;
+          done();
+        }, 500);
+      });
+
+      it('should call error callback if unsuccessful', function (done) {
+        var callbacks = [sinon.spy(), sinon.spy()];
+        this.treasure.addEvent('error', treasureHelper.properties, callbacks[0], callbacks[1]);
+        setTimeout(function() {
+          expect(callbacks[0].calledOnce).not.to.be.ok;
+          expect(callbacks[1].calledOnce).to.be.ok;
+          done();
+        }, 500);        
+      });
+
     });
     
-    describe("via Image Beacon to a fake server", function(){
+    // describe("via Image Beacon to a fake server", function(){
       
-      /*
-      beforeEach(function() {
-        this.treasure = new Treasure({
-          database: treasureHelper.database,
-          writeKey: treasureHelper.writeKey,
-          host: treasureHelper.host,
-          requestType: 'beacon'
-        });
-      });
       
-      it("should add an image tag", function(){
-        
-        var callbacks = [function(){ console.log('here'); }, sinon.spy()];
-        this.treasure.addEvent(treasureHelper.table, treasureHelper.properties, callbacks[0], callbacks[1]);
-        
-        var tag = document.getElementById("treasure-beacon");
-        //expect(tag).to.exist;
-        //expect(callbacks[0].calledOnce).to.be.ok;
-        
-      });
-      */
+    //   beforeEach(function() {
+    //     this.treasure = new Treasure({
+    //       database: treasureHelper.database,
+    //       writeKey: treasureHelper.writeKey,
+    //       host: treasureHelper.host,
+    //       requestType: 'beacon'
+    //     });
+    //   });
       
-    });
+    //   it("should add an image tag", function(){
+        
+    //     var callbacks = [function(){ console.log('here'); }, sinon.spy()];
+    //     this.treasure.addEvent(treasureHelper.table, treasureHelper.properties, callbacks[0], callbacks[1]);
+        
+    //     var tag = document.getElementById("treasure-beacon");
+    //     //expect(tag).to.exist;
+    //     //expect(callbacks[0].calledOnce).to.be.ok;
+        
+    //   });
+      
+      
+    // });
     
   });
 });
