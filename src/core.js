609db49ecc29a48d7becb67684b518d869939f68
César Andreu <cesarandreu@gmail.com>
2014-06-12 16:22:59 -0700

    style(core): update to follow styleguide

--- src/core.js
+++ src/core.js
@@ -10,13 +10,13 @@
 
   function _validate(config) {
     if (_isUndefined(config)) {
-      throw new Error("Check out our JavaScript SDK Usage Guide: http://docs.treasuredata.com/articles/javascript-sdk/");
+      throw new Error('Check out our JavaScript SDK Usage Guide: http://docs.treasuredata.com/articles/javascript-sdk/');
     }
     if (_isUndefined(config.database) || _type(config.database) !== 'String' || !config.database.length) {
-      throw new Error("Please provide a database");
+      throw new Error('Please provide a database');
     }
     if (!(/^[a-z0-9_]{3,255}$/.test(config.database))) {
-      throw new Error("Database must be between 3 and 255 characters and must consist only of lower case letters, numbers, and _");
+      throw new Error('Database must be between 3 and 255 characters and must consist only of lower case letters, numbers, and _');
     }
   }
 
@@ -66,7 +66,7 @@
 
   function _type(obj){
     var text = (obj && obj.constructor) ? obj.constructor.toString() : void 0;
-	  return (text) ? text.match(/function (.*)\(/)[1] : "Null";
+	  return (text) ? text.match(/function (.*)\(/)[1] : 'Null';
   }
 
   function _each(o, cb, s){
@@ -101,8 +101,8 @@
         match,
         pl     = /\+/g,  // Regex for replacing addition symbol with a space
         search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = str.split("?")[1];
+        decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); },
+        query  = str.split('?')[1];
 
     while (!!(match=search.exec(query))) {
       urlParams[decode(match[1])] = decode(match[2]);
@@ -146,35 +146,48 @@
     return this.client.endpoint + '/js/v3/event/' + this.client.database + path;
   }
 
-
   var _request = {
 
     xhr: function(method, url, headers, body, apiKey, success, error){
-      if (!apiKey) return Treasure.log('Please provide an apikey from https://console.treasuredata.com/users/current');
+      if (!apiKey) {
+        return Treasure.log('Please provide an apikey from https://console.treasuredata.com/users/current');
+      }
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
+        if (xhr.readyState === 4) {
           if (xhr.status >= 200 && xhr.status < 300) {
             var response;
             try {
               response = JSON.parse(xhr.responseText);
             } catch (e) {
               Treasure.log("Could not JSON parse HTTP response: " + xhr.responseText);
-              if (error) error(xhr, e);
+              if (error) {
+                error(xhr, e);
+              }
             }
-            if (success && response) success(response);
+            if (success && response) {
+              success(response);
+            }
           } else {
             Treasure.log("HTTP request failed.");
-            if (error) error(xhr, null);
+            if (error) {
+              error(xhr, null); 
+            }
           }
         }
       };
       xhr.open(method, url, true);
-      if (apiKey) xhr.setRequestHeader("X-TD-WRITE-KEY", apiKey);
-      if (body) xhr.setRequestHeader("Content-Type", "application/json");
+      if (apiKey) {
+        xhr.setRequestHeader('X-TD-WRITE-KEY', apiKey);
+      }
+      if (body) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
       if (headers) {
         for (var headerName in headers) {
-          if (headers.hasOwnProperty(headerName)) xhr.setRequestHeader(headerName, headers[headerName]);
+          if (headers.hasOwnProperty(headerName)) {
+            xhr.setRequestHeader(headerName, headers[headerName]);
+          }
         }
       }
       var toSend = body ? JSON.stringify(body) : null;
@@ -182,15 +195,17 @@
     },
 
     jsonp: function(url, apiKey, success, error){
-      if (!apiKey) return Treasure.log('Please provide an apikey from https://console.treasuredata.com/users/current');
-      if (apiKey && url.indexOf("api_key") < 0) {
-        var delimiterChar = url.indexOf("?") > 0 ? "&" : "?";
-        url = url + delimiterChar + "api_key=" + apiKey;
+      if (!apiKey) {
+        return Treasure.log('Please provide an apikey from https://console.treasuredata.com/users/current');
+      }
+      if (apiKey && url.indexOf('api_key') < 0) {
+        var delimiterChar = url.indexOf('?') > 0 ? '&' : '?';
+        url = url + delimiterChar + 'api_key=' + apiKey;
       }
 
-      var callbackName = "TreasureJSONPCallback" + new Date().getTime();
+      var callbackName = 'TreasureJSONPCallback' + new Date().getTime();
       while (callbackName in window) {
-        callbackName += "a";
+        callbackName += 'a';
       }
       var loaded = false;
       window[callbackName] = function (response) {
@@ -201,50 +216,58 @@
         // Remove this from the namespace
         window[callbackName] = undefined;
       };
-      url = url + "&jsonp=" + callbackName;
-      var script = document.createElement("script");
-      script.id = "treasure-jsonp";
+      url = url + '&callback=' + callbackName;
+      var script = document.createElement('script');
+      script.id = 'td-js-sdk-jsonp';
       script.src = url;
-      document.getElementsByTagName("head")[0].appendChild(script);
+      document.getElementsByTagName('head')[0].appendChild(script);
       // for early IE w/ no onerror event
       script.onreadystatechange = function() {
-        if (loaded === false && this.readyState === "loaded") {
+        if (loaded === false && this.readyState === 'loaded') {
           loaded = true;
-          if (error) error();
+          if (error) {
+            error();
+          }
         }
       };
       // non-ie, etc
       script.onerror = function() {
         if (loaded === false) { // on IE9 both onerror and onreadystatechange are called
           loaded = true;
-          if (error) error();
-        }
-      };
-    },
-
-    beacon: function(url, apiKey, success, error){
-      if (apiKey && url.indexOf("api_key") < 0) {
-        var delimiterChar = url.indexOf("?") > 0 ? "&" : "?";
-        url = url + delimiterChar + "api_key=" + apiKey;
-      }
-      var loaded = false, img = document.createElement("img");
-      img.onload = function() {
-        loaded = true;
-        if ('naturalHeight' in this) {
-          if (this.naturalHeight + this.naturalWidth === 0) {
-            this.onerror(); return;
+          if (error) {
+            error();
           }
-        } else if (this.width + this.height === 0) {
-          this.onerror(); return;
         }
-        if (success) success({created: true});
       };
-      img.onerror = function() {
-        loaded = true;
-        if (error) error();
-      };
-      img.src = url;
     }
+
+    // beacon: function(url, apiKey, success, error){
+    //   if (apiKey && url.indexOf('api_key') < 0) {
+    //     var delimiterChar = url.indexOf('?') > 0 ? '&' : '?';
+    //     url = url + delimiterChar + 'api_key=' + apiKey;
+    //   }
+    //   var loaded = false, img = document.createElement('img');
+    //   img.onload = function() {
+    //     loaded = true;
+    //     if ('naturalHeight' in this) {
+    //       if (this.naturalHeight + this.naturalWidth === 0) {
+    //         this.onerror(); return;
+    //       }
+    //     } else if (this.width + this.height === 0) {
+    //       this.onerror(); return;
+    //     }
+    //     if (success) {
+    //       success({created: true});
+    //     }
+    //   };
+    //   img.onerror = function() {
+    //     loaded = true;
+    //     if (error) {
+    //       error();
+    //     }
+    //   };
+    //   img.src = url;
+    // }
   };
 
 
@@ -254,7 +277,7 @@
 
   var Events = Treasure.Events = {
     on: function(name, callback) {
-      this.listeners || (this.listeners = {});
+      this.listeners = this.listeners || {};
       var events = this.listeners[name] || (this.listeners[name] = []);
       events.push({callback: callback});
       return this;
@@ -267,8 +290,10 @@
       }
       var events = this.listeners[name] || [];
       for (var i = events.length; i--;) {
-        if (callback && callback == events[i]['callback']) this.listeners[name].splice(i, 1);
-        if (!callback || events.length == 0) {
+        if (callback && callback === events[i].callback) {
+          this.listeners[name].splice(i, 1);
+        }
+        if (!callback || events.length === 0) {
           this.listeners[name] = void 0;
           delete this.listeners[name];
         }
@@ -276,11 +301,13 @@
       return this;
     },
     trigger: function(name) {
-      if (!this.listeners) return this;
+      if (!this.listeners) {
+        return this;
+      }
       var args = Array.prototype.slice.call(arguments, 1);
       var events = this.listeners[name] || [];
       for (var i = 0; i < events.length; i++) {
-        events[i]['callback'].apply(this, args);
+        events[i].callback.apply(this, args);
       }
       return this;
     }
@@ -302,7 +329,7 @@
   };
 
   Treasure.log = function(message) {
-    if (typeof console == "object") {
+    if (typeof console === 'object') {
       console.log('[Treasure]', message);
     }
   };
