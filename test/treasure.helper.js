56ebcb0df7226a96d053b53d4b8e31ec7bbdf992
César Andreu <cesarandreu@gmail.com>
2014-06-18 18:36:01 -0700

    test(tracking): use real server for tests

--- test/treasure.helper.js
+++ test/treasure.helper.js
@@ -6,7 +6,8 @@ window.treasureHelper = {
   writeKey: '91/96da3cfb876cc50724d0dddef670d95eea2a0018',
   protocol: 'http',
   host: 'in-staging.treasuredata.com',
-  jsonphost: 'localhost:9999',
+  // jsonphost: 'localhost:9999',
+  jsonphost: 'in-staging.treasuredata.com',
   table: 'test_tb_1',
   properties: { username: 'treasure', color: 'blue' },
   responses: {
