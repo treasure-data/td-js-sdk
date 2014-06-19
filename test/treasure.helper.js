720144cede332f5c7b86596e61d0efff961974b8
César Andreu <cesarandreu@gmail.com>
2014-06-18 19:21:50 -0700

    chore(timeout): increase timeout to 5 seconds

--- test/treasure.helper.js
+++ test/treasure.helper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.treasureHelper = {
-  TIMEOUT: 1000,
+  TIMEOUT: 5000,
   database: 'test_db_1',
   writeKey: '91/96da3cfb876cc50724d0dddef670d95eea2a0018',
   protocol: 'http',
