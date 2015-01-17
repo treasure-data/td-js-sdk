1824c3d1826f0fa1781cd47616a41860aeaba24c
Cesar Andreu <cesarandreu@gmail.com>
2015-01-16 16:59:55 -0800

    refactor(tasks): set config directly, use gulp loader, move sauce config to sauce.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,54 @@
 'use strict';
 
 var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
   runSequence = require('run-sequence'),
   browserify = require('browserify'),
   source = require('vinyl-source-stream'),
-  streamify = require('gulp-streamify'),
-  rename = require('gulp-rename'),
-  replace = require('gulp-replace'),
+  buffer = require('vinyl-buffer'),
   express = require('express'),
   path = require('path'),
-  uglify = require('gulp-uglify'),
-  gzip = require('gulp-gzip'),
   del = require('del'),
   karma = require('karma').server,
   _ = require('lodash'),
   async = require('async'),
   glob = require('glob'),
   wd = require('wd'),
-  config = require('./config'),
   server;
 
 gulp.task('clean', function (cb) {
-  del([path.join(config.folders.dist, '/*'), '!.*'], cb);
+  del([path.join('dist', '/*'), '!.*'], cb);
 });
 
 var tdTask = function tdTask (entry, name) {
   return function () {
     return browserify(entry).bundle()
       .pipe(source(name))
+      .pipe(buffer())
       .pipe(gulp.dest('dist'))
-      .pipe(streamify(uglify()))
-      .pipe(rename({extname: '.min.js'}))
+      .pipe($.uglify())
+      .pipe($.rename({extname: '.min.js'}))
       .pipe(gulp.dest('dist'));
   };
 };
-gulp.task('td', tdTask(config.browserify.index, 'td.js'));
-gulp.task('td.legacy', tdTask(config.browserify.legacy, 'td.legacy.js'));
+gulp.task('td', tdTask('./lib/index.js', 'td.js'));
+gulp.task('td.legacy', tdTask('./lib/index.legacy.js', 'td.legacy.js'));
 
 gulp.task('loader', function () {
   return gulp
-    .src(config.loader.src)
-    .pipe(replace('@URL', process.env.URL || '//td.js'))
-    .pipe(replace('@SDK_GLOBAL', process.env.SDK_GLOBAL || 'Treasure'))
-    .pipe(gulp.dest(config.folders.dist))
-    .pipe(uglify())
-    .pipe(rename({extname: '.min.js'}))
-    .pipe(gulp.dest(config.folders.dist));
+    .src('src/loader.js')
+    .pipe($.replace('@URL', process.env.URL || '//td.js'))
+    .pipe($.replace('@SDK_GLOBAL', process.env.SDK_GLOBAL || 'Treasure'))
+    .pipe(gulp.dest('dist'))
+    .pipe($.uglify())
+    .pipe($.rename({extname: '.min.js'}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compress', function () {
   return gulp.src('./dist/td*js')
-    .pipe(gzip())
-    .pipe(gulp.dest(config.folders.dist));
+    .pipe($.gzip())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', function () {
@@ -61,23 +58,23 @@ gulp.task('default', ['build']);
 
 gulp.task('dev', function (done) {
   var app = express();
-  app.use(express.static(path.resolve(__dirname, config.folders.test)));
+  app.use(express.static(path.resolve(__dirname, 'test')));
   app.use(function(req, res, next) {
     if (req.url.indexOf('callback')) {
       if (req.url.indexOf('error') > -1) {
-        res.status(400).jsonp(config.server.error);
+        res.status(400).jsonp({error: true});
       } else {
-        res.status(200).jsonp(config.server.success);
+        res.status(200).jsonp({created: true});
       }
     } else {
       next();
     }
   });
-  server = app.listen(config.server.port, done);
+  server = app.listen(9999, done);
 });
 
 gulp.task('tdd', ['build', 'dev'], function (done) {
-  gulp.watch(config.tdd.watch, ['build']);
+  gulp.watch('lib/**/*.js', ['build']);
   karma.start(require('./karma.conf.js')(), function (exitCode) {
     if (server) {
       server.close();
@@ -113,11 +110,12 @@ gulp.task('e2e', function (done) {
 });
 
 gulp.task('ci', ['build', 'dev'], function (done) {
-  var sauceConnectLauncher = require('sauce-connect-launcher'),
-    karmaConfig = _.assign({}, require('./karma.conf.js')(), config.sauce.karma),
-    count = Math.ceil(config.sauce.browsers.length / config.sauce.concurrency);
+  var sauce = require('./sauce'),
+    sauceConnectLauncher = require('sauce-connect-launcher'),
+    karmaConfig = _.assign({}, require('./karma.conf.js')(), sauce.karma),
+    count = Math.ceil(sauce.browsers.length / sauce.concurrency);
 
-  sauceConnectLauncher(config.sauce.connect, function (err, sauceConnectProcess) {
+  sauceConnectLauncher(sauce.connect, function (err, sauceConnectProcess) {
     if (err) {
       console.error('Error with sauceConnectLauncher', err);
       done(err);
@@ -126,16 +124,16 @@ gulp.task('ci', ['build', 'dev'], function (done) {
     }
 
     console.log('Started Sauce Connect Process');
-    console.log('Tests will be run in', config.sauce.browsers.length, 'browsers');
+    console.log('Tests will be run in', sauce.browsers.length, 'browsers');
     async.timesSeries(count, function(n, next) {
       var browser,
         idx,
         sauceConfig = _.assign({}, karmaConfig);
       sauceConfig.browsers = [];
       sauceConfig.customLaunchers = {};
-      for (var i = 0; i < config.sauce.concurrency; i++) {
-        idx = (n * config.sauce.concurrency) + i;
-        browser = config.sauce.browsers[idx];
+      for (var i = 0; i < sauce.concurrency; i++) {
+        idx = (n * sauce.concurrency) + i;
+        browser = sauce.browsers[idx];
         if (browser) {
           browser.base = 'SauceLabs';
           sauceConfig.customLaunchers[idx] = browser;
