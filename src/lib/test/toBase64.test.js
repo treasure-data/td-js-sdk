6868148849d51c4bc5af346282e657c74ce4ef9b
Cesar Andreu <cesarandreu@gmail.com>
2016-09-13 06:21:13 -0700

    Rename base64 util to toBase64

--- src/lib/test/toBase64.test.js
+++ src/lib/test/toBase64.test.js
@@ -1,13 +1,13 @@
 var Buffer = require('buffer').Buffer
 var test = require('tape-catch')
-var base64 = require('../base64')
+var toBase64 = require('../toBase64')
 
-test('base64.encode', function (t) {
+test('toBase64', function (t) {
   t.plan(2)
 
   var complex = '{"Приве́т नमस्ते שָׁלוֹם":"Приве́т नमस्ते שָׁלוֹם"}'
-  t.equal(base64.encode(complex), new Buffer(complex).toString('base64'))
+  t.equal(toBase64(complex), new Buffer(complex).toString('base64'))
 
   var languages = '中文 español deutsch English हिन्दी العربية português বাংলা русский 日本語 ਪੰਜਾਬੀ 한국어 தமிழ் עברית'
-  t.equal(base64.encode(languages), new Buffer(languages).toString('base64'))
+  t.equal(toBase64(languages), new Buffer(languages).toString('base64'))
 })
