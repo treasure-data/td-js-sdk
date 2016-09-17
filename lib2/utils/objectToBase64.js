a1092ee8548f97d989dccb49b40ec23e78410d65
Cesar Andreu <cesarandreu@gmail.com>
2016-09-16 18:48:01 -0700

    Remove unused files

--- lib2/utils/objectToBase64.js
+++
@@ -1,8 +0,0 @@
-/**
- * Convert an object to a base64 string
- */
-var JSON3 = require('json3')
-
-module.exports = function objectToBase64 (object) {
-  return (new Buffer(JSON3.stringify(object))).toString('base64')
-}
