576bd717c8e7f6041548923ab3f195932431e5d9
Cesar Andreu <cesarandreu@gmail.com>
2016-09-24 06:06:26 -0700

    Add loadClient

---
+++ src/loadClient.js
@@ -0,0 +1,51 @@
+var window = require('global/window')
+var createTreasure = require('./createTreasure')
+var assert = require('./lib/assert')
+var isArray = require('./lib/isArray')
+var isFunction = require('./lib/isFunction')
+var isString = require('./lib/isString')
+var ready = require('./lib/ready')
+// var log = require('./lib/log')
+var forEach = require('./lib/forEach')
+
+module.exports = function loadClient (name, scaffold) {
+  function client (action, input) {
+    if (isFunction(action)) {
+      ready(function () {
+        action(client.instance)
+      })
+    } else if (isString(action)) {
+      var inputParams = input || {}
+      switch (action) {
+        case 'create':
+          assert(!client.instance, 'already called create')
+          client.instance = createTreasure(inputParams)
+          break
+        case 'send':
+        case 'sendEvent':
+        case 'sendPageview':
+        case 'setGlobalContext':
+        case 'setTableContext':
+        case 'trackClicks':
+          assert(!!client.instance, 'must call create')
+          client.instance[action](inputParams)
+          break
+        default:
+          // log('unknown action', action)
+          break
+      }
+    } else {
+      // log('unexpected action', action)
+    }
+  }
+
+  client.instance = null
+
+  window[name] = client
+
+  if (scaffold && isArray(scaffold.q)) {
+    forEach(scaffold.q, function (args) {
+      client.apply(client, args)
+    })
+  }
+}
