70dfcedb45f75da9aa657dfe01bd02dc04ad16f7
Cesar Andreu <cesarandreu@gmail.com>
2016-12-15 22:05:40 -0800

    Fix CDN path (#41)

--- bin/upload-release.sh
+++ bin/upload-release.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 VERSION=$(cat $ROOT_DIR/package.json | jq -r '.version')
 
-aws s3 sync ./dist/ s3://td-cdn/js-sdk/$VERSION/ \
+aws s3 sync ./dist/ s3://td-cdn/sdk/$VERSION/ \
   --acl "public-read" \
   --cache-control "public, max-age=315360000" \
   --exclude "*loader.min.js" \
