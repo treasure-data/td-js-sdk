9b772674743eaba756415cc57a1599d0cb3f6c3e
MikeMac <mikemaccs@gmail.com>
2017-05-08 11:26:07 -0500

    Add build

--- dist/td.js
+++ dist/td.js
@@ -4037,10 +4037,10 @@
 	var jsonp = __webpack_require__(4)
 	var invariant = __webpack_require__(3).invariant
 	var noop = __webpack_require__(3).noop
-	var cookie = __webpack_require__(61)
+	var cookie = __webpack_require__(61).default
 
 	function cacheSuccess (result, cookieName) {
-	  cookie.set(cookieName, result['global_id'], { expires: 6000 })
+	  cookie.setItem(cookieName, result['global_id'], 6000)
 	  return result['global_id']
 	}
 
@@ -4052,7 +4052,7 @@
 	  error = error || noop
 
 	  var cookieName = this.client.globalIdCookie
-	  var cachedGlobalId = cookie.get(this.client.globalIdCookie)
+	  var cachedGlobalId = cookie.getItem(this.client.globalIdCookie)
 	  if (cachedGlobalId && !forceFetch) {
 	    return setTimeout(function () {
 	      success(cachedGlobalId)
@@ -4083,180 +4083,93 @@
 
 /***/ },
 /* 61 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
-	var __WEBPACK_AMD_DEFINE_RESULT__;/*
-	 * Cookies.js - 1.2.3
-	 * https://github.com/ScottHamper/Cookies
-	 *
-	 * This is free and unencumbered software released into the public domain.
-	 */
-	(function (global, undefined) {
-	    'use strict';
+	'use strict';
 
-	    var factory = function (window) {
-	        if (typeof window.document !== 'object') {
-	            throw new Error('Cookies.js requires a `window` with a `document` object');
-	        }
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var encode = function encode(val) {
+	  try {
+	    return encodeURIComponent(val);
+	  } catch (e) {
+	    console.error('error encode %o');
+	  }
+	  return null;
+	};
 
-	        var Cookies = function (key, value, options) {
-	            return arguments.length === 1 ?
-	                Cookies.get(key) : Cookies.set(key, value, options);
-	        };
+	var decode = function decode(val) {
+	  try {
+	    return decodeURIComponent(val);
+	  } catch (err) {
+	    console.error('error decode %o');
+	  }
+	  return null;
+	};
 
-	        // Allows for setter injection in unit tests
-	        Cookies._document = window.document;
+	var handleSkey = function handleSkey(sKey) {
+	  return encode(sKey).replace(/[\-\.\+\*]/g, '\\$&');
+	};
 
-	        // Used to ensure cookie keys do not collide with
-	        // built-in `Object` properties
-	        Cookies._cacheKeyPrefix = 'cookey.'; // Hurr hurr, :)
-	        
-	        Cookies._maxExpireDate = new Date('Fri, 31 Dec 9999 23:59:59 UTC');
-
-	        Cookies.defaults = {
-	            path: '/',
-	            secure: false
-	        };
-
-	        Cookies.get = function (key) {
-	            if (Cookies._cachedDocumentCookie !== Cookies._document.cookie) {
-	                Cookies._renewCache();
-	            }
-	            
-	            var value = Cookies._cache[Cookies._cacheKeyPrefix + key];
-
-	            return value === undefined ? undefined : decodeURIComponent(value);
-	        };
-
-	        Cookies.set = function (key, value, options) {
-	            options = Cookies._getExtendedOptions(options);
-	            options.expires = Cookies._getExpiresDate(value === undefined ? -1 : options.expires);
-
-	            Cookies._document.cookie = Cookies._generateCookieString(key, value, options);
-
-	            return Cookies;
-	        };
-
-	        Cookies.expire = function (key, options) {
-	            return Cookies.set(key, undefined, options);
-	        };
-
-	        Cookies._getExtendedOptions = function (options) {
-	            return {
-	                path: options && options.path || Cookies.defaults.path,
-	                domain: options && options.domain || Cookies.defaults.domain,
-	                expires: options && options.expires || Cookies.defaults.expires,
-	                secure: options && options.secure !== undefined ?  options.secure : Cookies.defaults.secure
-	            };
-	        };
-
-	        Cookies._isValidDate = function (date) {
-	            return Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime());
-	        };
-
-	        Cookies._getExpiresDate = function (expires, now) {
-	            now = now || new Date();
-
-	            if (typeof expires === 'number') {
-	                expires = expires === Infinity ?
-	                    Cookies._maxExpireDate : new Date(now.getTime() + expires * 1000);
-	            } else if (typeof expires === 'string') {
-	                expires = new Date(expires);
-	            }
-
-	            if (expires && !Cookies._isValidDate(expires)) {
-	                throw new Error('`expires` parameter cannot be converted to a valid Date instance');
-	            }
-
-	            return expires;
-	        };
-
-	        Cookies._generateCookieString = function (key, value, options) {
-	            key = key.replace(/[^#$&+\^`|]/g, encodeURIComponent);
-	            key = key.replace(/\(/g, '%28').replace(/\)/g, '%29');
-	            value = (value + '').replace(/[^!#$&-+\--:<-\[\]-~]/g, encodeURIComponent);
-	            options = options || {};
-
-	            var cookieString = key + '=' + value;
-	            cookieString += options.path ? ';path=' + options.path : '';
-	            cookieString += options.domain ? ';domain=' + options.domain : '';
-	            cookieString += options.expires ? ';expires=' + options.expires.toUTCString() : '';
-	            cookieString += options.secure ? ';secure' : '';
-
-	            return cookieString;
-	        };
-
-	        Cookies._getCacheFromString = function (documentCookie) {
-	            var cookieCache = {};
-	            var cookiesArray = documentCookie ? documentCookie.split('; ') : [];
-
-	            for (var i = 0; i < cookiesArray.length; i++) {
-	                var cookieKvp = Cookies._getKeyValuePairFromCookieString(cookiesArray[i]);
-
-	                if (cookieCache[Cookies._cacheKeyPrefix + cookieKvp.key] === undefined) {
-	                    cookieCache[Cookies._cacheKeyPrefix + cookieKvp.key] = cookieKvp.value;
-	                }
-	            }
-
-	            return cookieCache;
-	        };
-
-	        Cookies._getKeyValuePairFromCookieString = function (cookieString) {
-	            // "=" is a valid character in a cookie value according to RFC6265, so cannot `split('=')`
-	            var separatorIndex = cookieString.indexOf('=');
-
-	            // IE omits the "=" when the cookie value is an empty string
-	            separatorIndex = separatorIndex < 0 ? cookieString.length : separatorIndex;
-
-	            var key = cookieString.substr(0, separatorIndex);
-	            var decodedKey;
-	            try {
-	                decodedKey = decodeURIComponent(key);
-	            } catch (e) {
-	                if (console && typeof console.error === 'function') {
-	                    console.error('Could not decode cookie with key "' + key + '"', e);
-	                }
-	            }
-	            
-	            return {
-	                key: decodedKey,
-	                value: cookieString.substr(separatorIndex + 1) // Defer decoding value until accessed
-	            };
-	        };
-
-	        Cookies._renewCache = function () {
-	            Cookies._cache = Cookies._getCacheFromString(Cookies._document.cookie);
-	            Cookies._cachedDocumentCookie = Cookies._document.cookie;
-	        };
-
-	        Cookies._areEnabled = function () {
-	            var testKey = 'cookies.js';
-	            var areEnabled = Cookies.set(testKey, 1).get(testKey) === '1';
-	            Cookies.expire(testKey);
-	            return areEnabled;
-	        };
-
-	        Cookies.enabled = Cookies._areEnabled();
-
-	        return Cookies;
-	    };
-	    var cookiesExport = (global && typeof global.document === 'object') ? factory(global) : factory;
-
-	    // AMD support
-	    if (true) {
-	        !(__WEBPACK_AMD_DEFINE_RESULT__ = function () { return cookiesExport; }.call(exports, __webpack_require__, exports, module), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-	    // CommonJS/Node.js support
-	    } else if (typeof exports === 'object') {
-	        // Support Node.js specific `module.exports` (which can be a function)
-	        if (typeof module === 'object' && typeof module.exports === 'object') {
-	            exports = module.exports = cookiesExport;
-	        }
-	        // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
-	        exports.Cookies = cookiesExport;
-	    } else {
-	        global.Cookies = cookiesExport;
+	var Cookies = {
+	  getItem: function getItem(sKey) {
+	    if (!sKey) {
+	      return null;
 	    }
-	})(typeof window === 'undefined' ? this : window);
+	    return decode(document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*' + handleSkey(sKey) + '\\s*\\=\\s*([^;]*).*$)|^.*$'), '$1')) || null;
+	  },
+	  setItem: function setItem(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+	    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+	      return false;
+	    }
+	    var sExpires = '';
+	    if (vEnd) {
+	      switch (vEnd.constructor) {
+	        case Number:
+	          if (vEnd === Infinity) {
+	            sExpires = '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+	          } else {
+	            sExpires = '; max-age=' + vEnd;
+	          }
+	          break;
+	        case String:
+	          sExpires = '; expires=' + vEnd;
+	          break;
+	        case Date:
+	          sExpires = '; expires=' + vEnd.toUTCString();
+	          break;
+	        default:
+	          break;
+	      }
+	    }
+	    document.cookie = [encode(sKey), '=', encode(sValue), sExpires, sDomain ? '; domain=' + sDomain : '', sPath ? '; path=' + sPath : '', bSecure ? '; secure' : ''].join('');
+	    return true;
+	  },
+	  removeItem: function removeItem(sKey, sPath, sDomain) {
+	    if (!this.hasItem(sKey)) {
+	      return false;
+	    }
+	    document.cookie = [encode(sKey), '=; expires=Thu, 01 Jan 1970 00:00:00 GMT', sDomain ? '; domain=' + sDomain : '', sPath ? '; path=' + sPath : ''].join('');
+	    return true;
+	  },
+	  hasItem: function hasItem(sKey) {
+	    if (!sKey) {
+	      return false;
+	    }
+	    return new RegExp('(?:^|;\\s*)' + encode(sKey).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\=').test(document.cookie);
+	  },
+	  keys: function keys() {
+	    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, '').split(/\s*(?:\=[^;]*)?;\s*/);
+	    aKeys = aKeys.map(function (key) {
+	      return decode(key);
+	    });
+	    return aKeys;
+	  }
+	};
+
+	exports.default = Cookies;
+
 
 /***/ },
 /* 62 */
@@ -4271,7 +4184,7 @@
 	// Modules
 	var window = __webpack_require__(58)
 	var _ = __webpack_require__(8)
-	var cookie = __webpack_require__(61)
+	var cookie = __webpack_require__(61).default
 	var generateUUID = __webpack_require__(63)
 	var version = __webpack_require__(55)
 	var document = window.document
@@ -4414,10 +4327,10 @@
 	    tdClientIdSource = 'CONFIG'
 	  } else if (!config.clientId || !_.isString(config.clientId)) {
 	    if (config.storage && config.storage.name) {
-	      config.clientId = cookie.get(config.storage.name)
+	      config.clientId = cookie.getItem(config.storage.name)
 	      tdClientIdSource = 'COOKIE'
 	    }
-	    if (!config.clientId) {
+	    if (!config.clientId || config.clientId === 'undefined') {
 	      config.clientId = generateUUID()
 	      tdClientIdSource = 'GENERATED'
 	    }
@@ -4439,7 +4352,7 @@
 	    // When it's localhost, an IP, or custom domain, set the cookie directly
 	    if (is.ip || is.local || is.custom) {
 	      clone.domain = is.local ? null : clone.domain
-	      cookie(storage.name, uuid, clone)
+	      cookie.setItem(storage.name, uuid, clone.expiry, clone.path, clone.domain)
 	    } else {
 	      // Otherwise iterate recursively on the domain until it gets set
 	      // For example, if we have three sites:
@@ -4449,11 +4362,11 @@
 	      var domain = storage.domain.split('.')
 	      for (var i = domain.length - 1; i >= 0; i--) {
 	        clone.domain = (domain.slice(i).join('.'))
-	        cookie(storage.name, uuid, clone)
+	        cookie.setItem(storage.name, uuid, clone.expiry, clone.path, clone.domain)
 
 	        // Break when cookies aren't being cleared and it gets set properly
 	        // Don't break when uuid is falsy so all the cookies get cleared
-	        if (cookie.get(storage.name) && uuid) {
+	        if (cookie.getItem(storage.name) && uuid) {
 	          // When cookie is set succesfully, save used domain in storage object
 	          storage.domain = clone.domain
 	          break
@@ -4495,7 +4408,7 @@
 	      td_client_id_source: tdClientIdSource,
 	      td_document_cookie_original: tdDocumentCookieOriginal,
 	      td_document_cookie: document.cookie,
-	      td_cookie_value: this.client.storage && cookie.get(this.client.storage.name),
+	      td_cookie_value: this.client.storage && cookie.getItem(this.client.storage.name),
 	      td_client_id_original: tdClientIdOriginal
 	    }))
 	  }
