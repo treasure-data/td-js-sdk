1b313360503f7bf4d511cb8a5dac7c07abb710db
Michele Caramello <michele@treasure-data.com>
2017-05-22 18:22:49 -0700

    support --dryrun mode

--- bin/upload-release.sh
+++ bin/upload-release.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+DRYRUN=''
+if [ "$1" = '-d' ]; then
+  DRYRUN='--dryrun'
+fi
+
 set -euo pipefail
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-VERSION=$(cat $ROOT_DIR/package.json | jq -r '.version')
+VERSION=$(jq -r '.version' < "${ROOT_DIR}/package.json")
 
-aws s3 sync ./dist/ s3://td-cdn/sdk/$VERSION/ \
-  --acl "public-read" \
-  --cache-control "public, max-age=315360000" \
-  --exclude "*loader.min.js" \
-  --profile dev-fluentd --dryrun
+aws --profile dev-frontend                      \
+  s3 sync ./dist/ "s3://td-cdn/sdk/${VERSION}/" \
+    ${DRYRUN}                                   \
+    --acl 'public-read'                         \
+    --cache-control 'public, max-age=315360000' \
+    --exclude "*loader.min.js"
