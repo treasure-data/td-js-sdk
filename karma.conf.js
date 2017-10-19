f187a62ad6b7653efb64281c4be34603803c4d0b
Dan Reeves <hey@danreev.es>
2017-10-19 22:48:26 +0100

    do we actually need the polyfills without sinon?

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/es5.js',
       {pattern: 'lib/**/*.js', included: false},
       {pattern: 'test/*.spec.js', included: true, watched: false}
     ],
