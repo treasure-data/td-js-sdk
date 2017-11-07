8568d6301c74ab122470836172decd87e5d7ca74
Dan Reeves <hey@danreev.es>
2017-11-07 18:41:34 +0000

    build after npm install

--- dist/td.js
+++ dist/td.js
@@ -1774,15 +1774,17 @@
     };
 }, function(module, exports) {
     (function(global) {
+        var win;
         if (typeof window !== "undefined") {
-            module.exports = window;
+            win = window;
         } else if (typeof global !== "undefined") {
-            module.exports = global;
+            win = global;
         } else if (typeof self !== "undefined") {
-            module.exports = self;
+            win = self;
         } else {
-            module.exports = {};
+            win = {};
         }
+        module.exports = win;
     }).call(exports, function() {
         return this;
     }());
