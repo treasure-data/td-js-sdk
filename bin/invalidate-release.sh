3db8235c76c73c40b0e61bc3cb4b5df75fa493d9
MikeMac <mikemaccs@gmail.com>
2018-02-20 10:05:03 -0600

    Script for invalidating a specific release on cloudfront

---
+++ bin/invalidate-release.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+VERSION=$(jq -r '.version' < "${ROOT_DIR}/package.json")
+DRYRUN=''
+WAIT=0
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -d)
+      DRYRUN="--dryrun"
+      ;;
+    --version=*)
+      VERSION="${1#*=}"
+      ;;
+    --wait)
+      WAIT=1
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+JSON=$(aws --profile dev-frontend                            \
+  cloudfront create-invalidation                             \
+    --distribution-id E1F7ECRVBF3EX2                         \
+    --paths /sdk/${VERSION}/td.js /sdk/${VERSION}/td.min.js  \
+    ${DRYRUN}                                                \
+    --region 'us-east-2')
+
+INVALIDATION_ID=$(echo $JSON | jq -r '.Invalidation.Id')
+echo Created Invalidation: $INVALIDATION_ID
+
+if [ $WAIT -eq 1 ]; then
+  echo "Waiting until invalidation complete"
+  ./bin/check-invalidation.sh --id=$INVALIDATION_ID
+fi
