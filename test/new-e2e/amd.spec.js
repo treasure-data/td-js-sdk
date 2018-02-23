3c0b7e0e117371afced94d9df3cb688ef1ae0e2e
Dan Reeves <hey@danreev.es>
2018-02-23 15:06:09 +0000

    move test into spec file using executeAsync

--- test/new-e2e/amd.spec.js
+++ test/new-e2e/amd.spec.js
@@ -3,11 +3,26 @@ var expect = require('expect.js')
 describe('amd', function () {
   it('should be compatible with require.js', function () {
     browser.url('http://localhost:1337/fixtures/amd/index.html')
-    browser.waitUntil(function () {
-      return browser.getText('#status') === 'success'
-    }, 5000, 'expected TD status to be success after 5s');
+
+    // RequireJS Module should load an execute correctly
     browser.waitUntil(function () {
       return browser.getText('#rjs') === 'success'
     }, 5000, 'expected Require.js status to be success after 5s');
+
+    var result = browser.executeAsync(function (done) {
+      var td = new Treasure({
+        database: 'test_db_request',
+        writeKey: '91/96da3cfb876cc50724d0dddef670d95eea2a0018',
+        host: 'in-staging.treasuredata.com'
+      })
+      td.trackEvent('track_event', {date_string: (new Date()).toString()}, function () {
+        done(true)
+      }, function () {
+        done(false)
+      })
+    })
+
+    expect(result.value).to.be(true)
+
   })
 })
