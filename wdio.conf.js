136639e679ae3726b60af5ae12a7d8dbd6e8b12f
Dan Reeves <hey@danreev.es>
2018-02-28 14:45:59 +0000

    standard fix, remove unnecessary manual browserstack local setup

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,25 +1,29 @@
-var browserstack = require('browserstack-local')
 var browserlist = Object.values(require('./browserlist.json'))
 var args = process.argv.slice(2)
 var local = args.includes('--local')
 
 var build = require('child_process')
   .execSync('git rev-parse --short=9 HEAD', { cwd: __dirname })
-  .toString().trim()
+  .toString()
+  .trim()
 
 var services = local
   ? ['selenium-standalone', 'static-server']
   : ['browserstack', 'selenium-standalone', 'static-server']
 
-var capabilities = local ? [{
-  browserName: 'chrome'
-}] : browserlist.map(function (browser) {
-  return Object.assign({}, browser, {
-    'browserstack.local': true,
-    project: 'td-js-sdk',
-    build: build
+var capabilities = local
+  ? [
+    {
+      browserName: 'chrome'
+    }
+  ]
+  : browserlist.map(function (browser) {
+    return Object.assign({}, browser, {
+      'browserstack.local': true,
+      project: 'td-js-sdk',
+      build: build
+    })
   })
-})
 
 var localConfig = {
   host: 'localhost'
@@ -29,51 +33,37 @@ var browserstackConfig = {
   host: 'hub.browserstack.com',
   user: process.env.BROWSER_STACK_USERNAME,
   key: process.env.BROWSER_STACK_ACCESS_KEY,
-  // Code to start browserstack local before start of test
-  onPrepare: function (config, capabilities) {
-    console.log('Connecting local')
-    return new Promise(function (resolve, reject) {
-      exports.bs_local = new browserstack.Local()
-      exports.bs_local.start({ 'key': exports.config.key }, function (error) {
-        if (error) return reject(error)
-        console.log('Connected. Now testing...')
-        resolve()
-      })
-    })
-  },
-
-  // Code to stop browserstack local after end of test
-  onComplete: function (capabilties, specs) {
-    exports.bs_local.stop(function () {})
-  }
+  browserstackLocal: true
 }
 
-exports.config = Object.assign({}, {
-  capabilities: capabilities,
-  services: services,
-  specs: [
-    './test/new-e2e/*.js'
-  ],
-  staticServerFolders: [
-    { mount: '/fixtures', path: './test/fixtures' },
-    { mount: '/dist', path: './test/dist' },
-    { mount: '/assets', path: './test/assets' }
-  ],
-  maxInstances: 5,
-  sync: true,
-  logLevel: 'error',
-  coloredLogs: true,
-  deprecationWarnings: true,
-  bail: 0,
-  screenshotPath: './errorShots/',
-  baseUrl: '',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 90000,
-  connectionRetryCount: 3,
-  framework: 'mocha',
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 30000
+exports.config = Object.assign(
+  {},
+  {
+    capabilities: capabilities,
+    services: services,
+    specs: ['./test/new-e2e/*.js'],
+    staticServerFolders: [
+      { mount: '/fixtures', path: './test/fixtures' },
+      { mount: '/dist', path: './test/dist' },
+      { mount: '/assets', path: './test/assets' }
+    ],
+    maxInstances: 1,
+    sync: true,
+    logLevel: 'error',
+    coloredLogs: true,
+    deprecationWarnings: true,
+    bail: 0,
+    screenshotPath: './errorShots/',
+    baseUrl: '',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    mochaOpts: {
+      ui: 'bdd',
+      timeout: 30000
+    },
+    staticServerPort: 1337
   },
-  staticServerPort: 1337
-}, local ? localConfig : browserstackConfig)
+  local ? localConfig : browserstackConfig
+)
