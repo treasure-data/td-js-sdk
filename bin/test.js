9dcaee1fbae7abac39d871f398e60acbf605ae51
Dan Reeves <hey@danreev.es>
2018-03-13 18:41:47 +0000

    improve bin/test file to run unit and e2e tests

--- bin/test.js
+++ bin/test.js
@@ -2,12 +2,22 @@
 
 var cp = require('child_process')
 
-// var runBrowserTests = process.env.CI &&
-//   !process.env.TRAVIS_PULL_REQUEST ||
-//   process.env.TRAVIS_PULL_REQUEST === 'false'
+function npm (command) {
+  return cp.spawn('npm', ['run', command], { stdio: 'inherit' })
+}
 
-var script = process.env.CI ? 'test-ci' : 'test-local'
-var node = cp.spawn('npm', ['run', script], { stdio: 'inherit' })
-node.on('close', function (code) {
-  process.exit(code)
+var unittests = process.env.CI ? 'test-ci' : 'test-local'
+var e2etests = process.env.CI ? 'e2e-full' : 'e2e-local'
+
+var unit = npm(unittests)
+
+unit.on('close', function (code) {
+  if (code === 0) {
+    var e2e = npm(e2etests)
+    e2e.on('close', function (code) {
+      process.exit(code)
+    })
+  } else {
+    process.exit(code)
+  }
 })
