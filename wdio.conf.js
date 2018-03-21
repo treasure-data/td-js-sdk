2172549599b26533651235d09e39faf69aabd9b0
Dan Reeves <hey@danreev.es>
2018-03-21 20:35:01 +0000

    Revert "filter out the real devices :/"
    This reverts commit 3b79df44a6b7ff2cde040630a21d9c331cffc733.

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,8 +23,7 @@ var capabilities = local
       project: 'td-js-sdk',
       build: build
     })
-  }).filter(browser => browser.real_mobile !== 'true')
-  // TODO Don't filter out real devices
+  })
 
 var localConfig = {
   host: 'localhost'
