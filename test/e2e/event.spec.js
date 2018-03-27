4e16f978b4ea2450a3494acfe9353f34c1afcfae
Dan Reeves <hey@danreev.es>
2018-03-27 18:53:59 +0100

    remove uneeded fixtures

--- test/e2e/event.spec.js
+++ test/e2e/event.spec.js
@@ -3,7 +3,7 @@ var expect = require('expect.js')
 
 describe('trackEvent', function () {
   it('is a smoketest', async function () {
-    await browser.url('http://localhost:1337/fixtures/event/index.html')
+    await browser.url('http://localhost:1337/fixtures/generic/index.html')
     await browser.timeouts('script', 5000)
 
     var result = await browser.executeAsync(function (done) {
