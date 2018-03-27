a735eae9ee59fb4e00a99e86c7706c737803508a
Dan Reeves <hey@danreev.es>
2018-03-27 15:12:01 +0100

    only run e2e

--- bin/test.js
+++ bin/test.js
@@ -6,18 +6,18 @@ function npm (command) {
   return cp.spawn('npm', ['run', command], { stdio: 'inherit' })
 }
 
-var unittests = process.env.CI ? 'test-ci' : 'test-local'
+// var unittests = process.env.CI ? 'test-ci' : 'test-local'
 var e2etests = process.env.CI ? 'e2e-full' : 'e2e-local'
 
-var unit = npm(unittests)
+// var unit = npm(unittests)
 
-unit.on('close', function (code) {
-  if (code === 0) {
-    var e2e = npm(e2etests)
-    e2e.on('close', function (code) {
-      process.exit(code)
-    })
-  } else {
-    process.exit(code)
-  }
+// unit.on('close', function (code) {
+// if (code === 0) {
+var e2e = npm(e2etests)
+e2e.on('close', function (code) {
+  process.exit(code)
 })
+// } else {
+// process.exit(code)
+// }
+// })
