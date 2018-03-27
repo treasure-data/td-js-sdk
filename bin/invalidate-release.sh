d52628ef9dea48bfcdea7218770c4209f86f9313
MikeMac <mikemaccs@gmail.com>
2018-03-27 02:03:32 -0700

    No Dry Runs in invalidate-release

--- bin/invalidate-release.sh
+++ bin/invalidate-release.sh
@@ -3,14 +3,10 @@
 set -euo pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 VERSION=$(jq -r '.version' < "${ROOT_DIR}/package.json")
-DRYRUN='--dryrun'
 WAIT=0
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    -f|--force)
-      DRYRUN=""
-      ;;
     --version=*)
       VERSION="${1#*=}"
       ;;
@@ -30,7 +26,6 @@ JSON=$(aws --profile dev-frontend                            \
   cloudfront create-invalidation                             \
     --distribution-id E1F7ECRVBF3EX2                         \
     --paths /sdk/${VERSION}/td.js /sdk/${VERSION}/td.min.js  \
-    ${DRYRUN}                                                \
     --region 'us-east-2')
 
 INVALIDATION_ID=$(echo $JSON | jq -r '.Invalidation.Id')
