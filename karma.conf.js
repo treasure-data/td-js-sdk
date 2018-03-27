2462e8a556ae0a37ed92b49921b051bafdafe4f5
Dan Reeves <hey@danreev.es>
2018-03-27 13:59:52 +0100

    add browserstack reporter to karma, so tests are marked as failed

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['min'],
+    reporters: ['min', 'BrowserStack'],
 
     // web server port
     port: 9876,
