14fc90a105127b16a542ca73330b1e7681c03af4
Dan Reeves <hey@danreev.es>
2018-05-02 19:25:48 +0100

    disable unit tests

--- bin/test.js
+++ bin/test.js
@@ -9,15 +9,15 @@ function npm (command) {
 var unittests = process.env.CI ? 'test-ci' : 'test-local'
 var e2etests = process.env.CI ? 'e2e-full' : 'e2e-local'
 
-var unit = npm(unittests)
+// var unit = npm(unittests)
 
-unit.on('close', function (code) {
-  if (code === 0) {
-    var e2e = npm(e2etests)
-    e2e.on('close', function (code) {
-      process.exit(code)
-    })
-  } else {
-    process.exit(code)
-  }
+// unit.on('close', function (code) {
+// if (code === 0) {
+var e2e = npm(e2etests)
+e2e.on('close', function (code) {
+  process.exit(code)
 })
+// } else {
+// process.exit(code)
+// }
+// })
