bf405ca5791c74b4042411da108e724bd68b9b25
MikeMac <mikemaccs@gmail.com>
2018-05-24 17:44:03 -0500

    2.1.0

--- dist/td.js
+++ dist/td.js
@@ -2168,7 +2168,7 @@
 }, function(module, exports) {
     module.exports = {
         "GLOBAL": "Treasure",
-        "VERSION": "2.1.0-1",
+        "VERSION": "2.1.0",
         "HOST": "in.treasuredata.com",
         "DATABASE": "",
         "PATHNAME": "/js/v3/event/"
