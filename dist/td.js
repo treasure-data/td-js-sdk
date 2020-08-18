e442245e9422d7a0d7a0f9a7352fc8925c4f0514
sondo <son.do@treasure-data.com>
2020-08-18 14:39:39 +0700

    release 2.4.1

--- dist/td.js
+++ dist/td.js
@@ -2270,7 +2270,7 @@
 }, function(module, exports) {
     module.exports = {
         "GLOBAL": "Treasure",
-        "VERSION": "2.4.0",
+        "VERSION": "2.4.1",
         "HOST": "in.treasuredata.com",
         "DATABASE": "",
         "PATHNAME": "/js/v3/event/"
