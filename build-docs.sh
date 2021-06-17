7a4a3131de0d162fae4f84e3417048928190d593
Austin Blackstone <austin.blackstone@TD-0250.local>
2021-06-17 19:25:08 +0900

    Initial commit with JSDoc comments on all functions, build scripts and jsdoc config

---
+++ build-docs.sh
@@ -0,0 +1,7 @@
+#! bash
+cd lib
+jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js > ../js-sdk-api.md
+
+# local helper, delete before committing
+rm ~/code/TD-API-Documentation/docs/js-sdk/js-sdk-api.md
+cp ~/code/td-js-sdk/js-sdk-api.md ~/code/TD-API-Documentation/docs/js-sdk/js-sdk-api.md
