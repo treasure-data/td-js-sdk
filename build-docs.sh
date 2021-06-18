7526f434b3b1dc85b5af66d02e16c281ebe65469
Austin Blackstone <austin.blackstone@TD-0250.local>
2021-06-18 21:38:35 +0900

    updated format on jsdoc exporters

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,7 @@
 #! bash
 cd lib
-jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js --partial ../scope.hbs --partial ../summary.hbs > ../js-sdk-api.md
+jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js --partial ../scope.hbs --partial ../summary.hbs --partial ../global-index.hbs > ../js-sdk-api.md
 
 # local helper, delete before committing
 rm ~/code/TD-API-Documentation/docs/js-sdk/js-sdk-api.md
-cp ~/code/td-js-sdk/js-sdk-api.md ~/code/TD-API-Documentation/docs/js-sdk/js-sdk-api.md
+cp ~/code/td-js-sdk/js-sdk-api.md ~/code/TD-API-Documentation/docs/js-sdk/api.md
