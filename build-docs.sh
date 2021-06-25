43b4d439d9114f667b3f64339eb1335df751f2aa
Austin Blackstone <austin.blackstone@TD-0250.local>
2021-06-26 03:15:54 +0900

    removing local build paths

--- build-docs.sh
+++ build-docs.sh
@@ -1,7 +1,3 @@
 #! bash
 cd lib
 jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js --partial ../scope.hbs --partial ../summary.hbs --partial ../global-index.hbs > ../js-sdk-api.md
-
-# local helper, delete before committing
-rm ~/code/TD-API-Documentation/docs/js-sdk/js-sdk-api.md
-cp ~/code/td-js-sdk/js-sdk-api.md ~/code/TD-API-Documentation/docs/js-sdk/api.md
