c7d2bd460ae30609886de11d2d31d1b6e69d40eb
sondo <son.do@treasure-data.com>
2021-07-02 09:30:09 +0700

    use npm to generate docs

--- build-docs.sh
+++
@@ -1,3 +0,0 @@
-#! bash
-cd lib
-jsdoc2md --configure ../jsdoc-config.json --files "." --no-cache --separators --example-lang js --partial ../scope.hbs --partial ../summary.hbs --partial ../global-index.hbs > ../js-sdk-api.md
