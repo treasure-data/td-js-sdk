2394fde3bf14be79174d0e65296228e5dd0241af
dnson <son.do@treasure-data.com>
2026-03-11 05:15:49 +0700

    release v4.4.2 (#317)

--- test/treasure.fetch_cookies.spec.js
+++ test/treasure.fetch_cookies.spec.js
@@ -212,46 +212,6 @@ describe('Fetch cookies and params', function () {
       expect(globalTable['ly_su']).to.equal('ly_su_value')
     })
 
-    it('should collect _ly_c from URL parameter when cookie is missing', function () {
-      // Mock window.location.search
-      var originalLocation = window.location
-      delete window.location
-      window.location = { search: '?_ly_c=param_value' }
-
-      treasure.collectTags({
-        vendors: ['yahoojp_ads']
-      })
-
-      var globalTable = treasure.get('$global')
-
-      expect(globalTable['ly_c']).to.equal('param_value')
-
-      // Restore
-      window.location = originalLocation
-    })
-
-    it('should prioritize cookie over parameter for _ly_c', function () {
-      // Set cookie
-      cookies.setItem('_ly_c', 'cookie_value', 5)
-
-      // Mock URL parameter
-      var originalLocation = window.location
-      delete window.location
-      window.location = { search: '?_ly_c=param_value' }
-
-      treasure.collectTags({
-        vendors: ['yahoojp_ads']
-      })
-
-      var globalTable = treasure.get('$global')
-
-      // Cookie should override parameter
-      expect(globalTable['ly_c']).to.equal('cookie_value')
-
-      // Restore
-      window.location = originalLocation
-    })
-
     it('should maintain backward compatibility with existing Yahoo tags', function () {
       setYahooJapanLegacyCookies()
 
